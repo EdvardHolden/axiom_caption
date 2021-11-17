dee785ed8ea7aaa79aa80ca0a9a024380fb99357
EdvardHolden <edvardholden@gmail.com>
2021-11-17 14:15:26 +0100

    Adapt tokenizer to the axiom format
    A bit unclear wheter the correct regex-group is extracted for all the axioms

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-
+# TODO are these needed?
 PROVERS = ['iprover', 'e', 'vampire']
 COMPETITIONS = ['hl4', 'jjt']
 
@@ -12,3 +12,17 @@ COMPETITION_RESULTS ={'jjt': {
         'iprover': 'http://www.tptp.org/CASC/J10/WWWFiles/Results/HL4/iProver---LTB-3.3/'
     }
 }
+
+
+
+# #############
+BATCH_SIZE = 32
+
+train_id_file = 'data/vampire_jjt_fof/train.txt'
+proof_data = 'data/raw/vampire_jjt.pkl'
+
+TOKEN_PAD = '<pad>'
+TOKEN_START = '<start>'
+TOKEN_END = '<end>'
+TOKEN_OOV = '<pad>'
+TOKEN_DELIMITER = '\n'
