b5a3a9211ec9255a5c9648dc5013fd40c7be80ff
EdvardHolden <edvardholden@gmail.com>
2021-12-09 22:00:37 +0100

    Add sampling function: greedy, temperature and top_k

--- evaluate.py
+++ evaluate.py
@@ -21,11 +21,80 @@ parser.add_argument(
 )
 parser.add_argument("--proof_data", default=config.proof_data, help="File containing the image descriptions")
 
+# Sampling options
+parser.add_argument(
+    "--sampler",
+    default="greedy",
+    choices=["greedy", "temperature", "top_k"],
+    help="The method used to sample the next word in the prediction",
+)
+parser.add_argument(
+    "--no_samples",
+    default=1,
+    type=int,
+    help="The number of samples to draw at each iteration (only on is passed to the model)",
+)
+parser.add_argument(
+    "--sampler_temperature",
+    default=1.0,
+    type=float,
+    help="The temperature when using the temperature sampler (0, 1]",
+)
+parser.add_argument(
+    "--sampler_top_k",
+    default=10,
+    type=int,
+    help="The top k predictions to use when recomputing the prediction distributions",
+)
+
 parser.add_argument("--max_length", default=None, type=int, help="The maximum length of the predictions")
 parser.add_argument("-v", "--verbose", action="count", default=0)
-parser.add_argument('-k', '--top_k', default=1, type=int, help='The top k words to extract at each loop iteration')
+
 
 # TODO maybe add some BeamSearch in here?
+
+
+def greedy_sampler(pred, no_samples):
+
+    # Get the top K predictions (negate values as argsort is ascending)
+    return np.argsort(-pred)[0][0:no_samples]
+
+
+def temperature_sampler(pred, no_samples, temp=1.0):
+
+    # Scale numbers by the temperature
+    pred = np.asarray(softmax(pred[0])).astype("float64")
+    # pred = np.asarray(pred).astype("float64")
+    conditional_probability = np.log(pred) / temp
+    # Compute the softmax of the new distribution
+    exp_preds = np.exp(conditional_probability)
+    conditional_probability = exp_preds / np.sum(exp_preds)
+    # Perform no_samples picking experiments from the new distribution
+    prob = np.random.multinomial(no_samples, conditional_probability, 1)
+
+    # Greedily get the top distributed samples
+    return greedy_sampler(prob, no_samples)
+
+
+def softmax(z):
+    return np.exp(z) / sum(np.exp(z))
+
+
+def top_k_sampler(pred, no_samples, k=10):
+
+    # Get the k-top words
+    top_k_probabilities, top_k_indices = tf.math.top_k(softmax(pred[0]), k=k, sorted=True)
+    # Compute the softmax of the top indices
+    top_k_indices = np.asarray(top_k_indices).astype("int32")
+    top_k_redistributed_probability = softmax(np.log(top_k_probabilities))
+    top_k_redistributed_probability = np.asarray(top_k_redistributed_probability).astype("float32")
+
+    # Sample no_samples without replacement
+    sampled_token = np.random.choice(
+        top_k_indices, size=no_samples, replace=False, p=top_k_redistributed_probability
+    )
+
+    return sampled_token
 
 
 def coverage_score(actual, predicted):
@@ -34,7 +103,9 @@ def coverage_score(actual, predicted):
     return np.average(scores)
 
 
-def generate_step(tokenizer, model, max_len, top_k, img_tensor):
+def generate_step(
+    tokenizer, model, max_len, img_tensor, sampler, no_samples, sampler_temperature, sampler_top_k
+):
 
     # List for storing predicted sequence
     result = []
@@ -48,27 +119,36 @@ def generate_step(tokenizer, model, max_len, top_k, img_tensor):
     # Run the model until we reach the max length or the end token
     for i in range(max_len):
         # Predict probabilities
-        predictions = model([img_tensor, dec_input])
+        pred = model([img_tensor, dec_input])
 
-        # Get the top K predictions (negate values as argsort is ascending)
-        top_predictions = np.argsort(-predictions)[0][0:top_k]
+        # Sample the next token(s)
+        if sampler == "greedy":
+            predictions = greedy_sampler(pred, no_samples)
+        elif sampler == "temperature":
+            predictions = temperature_sampler(pred, no_samples, temp=sampler_temperature)
+        elif sampler == "top_k":
+            predictions = top_k_sampler(pred, no_samples, k=sampler_top_k)
+        else:
+            raise ValueError(f"Unrecognised sampler '{sampler}'")
 
         # Return sequence if we predicted the end token
-        if tokenizer.index_word[top_predictions[0]] == config.TOKEN_END:
+        if tokenizer.index_word[predictions[0]] == config.TOKEN_END:
             return result
 
         # Add predicted IDs to the result
-        for pred_id in top_predictions:
+        for pred_id in predictions:
             result.append(tokenizer.index_word[pred_id])
 
         # Set the top predicted word as the next model input
-        dec_input = tf.expand_dims([top_predictions[0]], 0)
+        dec_input = tf.expand_dims([predictions[0]], 0)
 
     # Reached max length, returning the sequence
     return result
 
 
-def evaluate_model(tokenizer, model, test_data, max_len, top_k, verbose=0):
+def evaluate_model(
+    tokenizer, model, test_data, max_len, sampler, no_samples, sampler_temperature, sampler_top_k, verbose=0
+):
 
     # Initialise results
     actual, predicted = list(), list()
@@ -76,7 +156,9 @@ def evaluate_model(tokenizer, model, test_data, max_len, top_k, verbose=0):
     # Predict for each problem in the data
     for (_, (img_tensor, caption)) in tqdm(enumerate(test_data)):
         # Generate caption based on the image tensor
-        yhat = generate_step(tokenizer, model, max_len, top_k, img_tensor)
+        yhat = generate_step(
+            tokenizer, model, max_len, img_tensor, sampler, no_samples, sampler_temperature, sampler_top_k
+        )
 
         # Extract the string value from the tensor, remove start/end tokens,
         # convert to utf-8 and make into array
@@ -114,10 +196,9 @@ def main():
         max_len = args.max_length
     print("Max caption length: ", max_len)
 
-
     # Get the axiom ordering from the model parameter file
     axiom_order = get_model_params("experiments/base_model").axiom_order
-    print('Using axiom order: ', axiom_order)
+    print("Using axiom order: ", axiom_order)
 
     # Get the test dataset with batch 1 as we need to treat each caption separately
     # Also, we want the raw text so not providing a tokenizer
@@ -129,9 +210,18 @@ def main():
     model_dir = os.path.join(args.model_dir, "ckpt_dir")
     loaded_model = load_model(model_dir)
 
-
     # Run evaluation
-    scores = evaluate_model(tokenizer, loaded_model, test_data, max_len, args.top_k, verbose=args.verbose)
+    scores = evaluate_model(
+        tokenizer,
+        loaded_model,
+        test_data,
+        max_len,
+        args.sampler,
+        args.no_samples,
+        args.sampler_temperature,
+        args.sampler_top_k,
+        verbose=args.verbose,
+    )
     print("# Scores ")
     for score in sorted(scores):
         print(f"{score:<8}: {scores[score]:.2f}")
