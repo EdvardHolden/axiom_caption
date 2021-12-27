6d86e78479d17cdba5a2fc7df4bbbc4afa8c61c7
EdvardHolden <edvardholden@gmail.com>
2021-12-27 17:15:49 +0100

    Update with new parameters and nre argparser setup

--- search_hyperparams.py
+++ search_hyperparams.py
@@ -9,32 +9,53 @@ import config
 from tqdm import tqdm
 import utils
 
-
-# TODO should include options for including stuff about which embedding/dataset split to use here
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--parent_dir", default="experiments/learning_rate", help="Directory for reporting the model experiments"
-)
-parser.add_argument(
-    "--parameter_space",
-    type=str,
-    default="hyperparameter_space/example.json",
-    help="Path to json file describing the parameter space",
-)
+from train import get_train_parser
 
 
-# TODO this could be combined with the list of filtering arguments in a single function.
-def launch_training_job(job_dir):
+def get_hyperparam_parser():
     """
-    Launch training of the model with a set of hyperparameters in parent_dir/job_name
+    This function extends the training parser with the parameters
+    required for tuning the hyperparameters.
+    """
+
+    # Get the parser for the train script
+    parser = get_train_parser()
+
+    # Extract and store its original options for later use
+    train_parameters = sorted(parser.parse_args([]).__dict__.keys())
+
+    # Extend the argument parser
+    parser.add_argument(
+        "--experiment_dir",
+        default="experiments/learning_rate",
+        help="Directory for reporting the model experiments",
+    )
+    parser.add_argument(
+        "--parameter_space",
+        type=str,
+        default="hyperparameter_space/example.json",
+        help="Path to json file describing the parameter space",
+    )
+    return parser, train_parameters
+
+
+def launch_training_job(job_dir, args, training_parameters):
+    """
+    Launch training of the model with a set of hyperparameters in experiment_dir/job_name
     """
     cmd = f"{config.PYTHON} train.py --model_dir {job_dir}"
-    print(cmd)
+
+    # Add all other remaining training parameters
+    for param in training_parameters:
+        if param != "model_dir":
+            cmd += f" --{param} {args.__dict__[param]} "
+
     check_call(cmd, shell=True, stdout=subprocess.DEVNULL)
 
 
 def main():
 
+    parser, training_parameters = get_hyperparam_parser()
     args = parser.parse_args()
 
     # Define the hyper-parameter space
@@ -50,13 +71,12 @@ def main():
         # Make config description
         r = sorted(param_config.items())
         job_name = "_".join([p + "_" + str(v) for p, v in r])
-        print(job_name)
 
         # Create job dir
-        job_dir = utils.create_job_dir(args.parent_dir, job_name, params=param_config)
+        job_dir = utils.create_job_dir(args.experiment_dir, job_name, params=param_config)
 
         # Launch job
-        launch_training_job(job_dir)
+        launch_training_job(job_dir, args, training_parameters)
 
 
 if __name__ == "__main__":
