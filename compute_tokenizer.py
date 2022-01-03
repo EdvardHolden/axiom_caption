15a7161bfae9504329a88fff7e901131e55b1740
EdvardHolden <edvardholden@gmail.com>
2022-01-03 18:55:44 +0100

    Report correct length when not the whole vocab is used

--- compute_tokenizer.py
+++ compute_tokenizer.py
@@ -13,6 +13,7 @@ parser.add_argument("--proof_data", default=config.proof_data, help="File contai
 parser.add_argument(
     "--vocab_word_limit",
     default=None,
+    type=int,
     help="Number of top K words to include in the vocabulary. None for all words",
 )
 
@@ -26,8 +27,7 @@ def create_tokenizer(descriptions, vocab_word_limit):
     return tokenizer
 
 
-if __name__ == "__main__":
-
+def main():
     args = parser.parse_args()
 
     train_descriptions = load_clean_descriptions(
@@ -39,11 +39,17 @@ if __name__ == "__main__":
     tokenizer.word_index[config.TOKEN_PAD] = 0
     tokenizer.index_word[0] = config.TOKEN_PAD
 
-    # print('Vocabulary Size: {0}'.format(len(tokenizer.word_index) + 1))
-    print("Vocabulary Size: {0}".format(len(tokenizer.word_index)))
+    vocab_size = tokenizer.get_config()['num_words']
+    if vocab_size is None:
+        vocab_size = len(tokenizer.word_index)
+    print(f"Vocabulary Size: {vocab_size}")
 
     # Save the tokenizer
     save_path = os.path.join(os.path.dirname(args.id_file), "tokenizer.json")
     with open(save_path, "w") as f:
         json.dump(tokenizer.to_json(), f)
     print("Saved tokenizer to: ", save_path)
+
+
+if __name__ == "__main__":
+    main()
