59385cf9853c7685275f0042e06bcd6f577947bf
EdvardHolden <edvardholden@gmail.com>
2022-01-06 17:39:30 +0100

    Add forced rerunning to the setup

--- search_hyperparams.py
+++ search_hyperparams.py
@@ -1,6 +1,6 @@
 """Peform hyperparemeters search"""
 
-import argparse
+import os
 import itertools
 import json
 import subprocess
@@ -36,6 +36,12 @@ def get_hyperparam_parser():
         default="hyperparameter_space/example.json",
         help="Path to json file describing the parameter space",
     )
+    parser.add_argument(
+        "--rerun",
+        default=False,
+        action="store_true",
+        help="Force rerunning of a config even if the job dir already exists",
+    )
     return parser, train_parameters
 
 
@@ -66,17 +72,27 @@ def main():
     hp_parameters = [dict(zip(hp_space, v)) for v in itertools.product(*hp_space.values())]
 
     # Iterate over each param config
+    no_skipped_runs = 0
     for param_config in tqdm(hp_parameters):
 
         # Make config description
-        r = sorted(param_config.items())
-        job_name = "_".join([p + "_" + str(v) for p, v in r])
+        job_name = "_".join([p + "_" + str(v) for p, v in sorted(param_config.items())])
+
+        # If we are not forcing reruns and the jobdir already exists, we skip this configuration
+        if not args.rerun and os.path.exists(os.path.join(args.experiment_dir, job_name)):
+            no_skipped_runs += 1
+            print(f"Skipping rerun of: {job_name}")
+            continue
 
         # Create job dir
         job_dir = utils.create_job_dir(args.experiment_dir, job_name, params=param_config)
 
         # Launch job
         launch_training_job(job_dir, args, training_parameters)
+
+    # Report the number of skipped runs if any
+    if no_skipped_runs > 0:
+        print(f"Skipped a total of {no_skipped_runs} job runs")
 
 
 if __name__ == "__main__":
