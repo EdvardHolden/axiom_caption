8c35f032200a5e8ff745cb78a476194fd84e7bb2
EdvardHolden <edvardholden@gmail.com>
2022-04-05 14:32:04 +0200

    Fix vocab_size for tokenizer without word limit

--- dataset.py
+++ dataset.py
@@ -25,7 +25,12 @@ def get_tokenizer(tokenizer_path, verbose=0):
     # Function for loading the tokenizer from path
     with open(tokenizer_path) as f:
         tokenizer = tokenizer_from_json(json.load(f))
+
+    # Get the vocab size - might not already be set
     vocab_size = tokenizer.num_words
+    if vocab_size is None:
+        vocab_size = len(tokenizer.word_index)
+
     if verbose > 0:
         print("Vocabulary Size: %d" % vocab_size)
     return tokenizer, vocab_size
