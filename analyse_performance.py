9e6ac8c127b0a4574b225c8a019682c61b97980a
Edvard Holden <edvardholden@gmail.com>
2022-06-29 00:24:59 +0100

    Add init stats computation

--- analyse_performance.py
+++ analyse_performance.py
@@ -13,8 +13,8 @@ from online.get_scores import get_problem_stats, get_solved_problem_name_time
 # The direcotry to the proof axioms
 PROOF_AXIOMS = 'generated_problems/analysis/output_original_positive_axioms/'
 
-# TODO add upper bound as well?
-CONFIGS = {115554: 'generated_problems/analysis/output_original_clean/',
+CONFIGS = {115498: 'generated_problems/analysis/output_original_ideal', # Upper bound
+           115554: 'generated_problems/analysis/output_original_clean/', # Raw merged problem
            115555: 'generated_problems/analysis/output_original_sine_1_1/',
            115556: 'generated_problems/analysis/output_original_sine_3_0/'}
 
@@ -31,15 +31,19 @@ def get_metrics(problem_paths):
 
         # Load proof axioms
         proof = load_and_process_problem(os.path.join(PROOF_AXIOMS, name), deepmath=False)
+        # Hack: check if quoting / $distinct number axioms is added. if so, remove ut
+        if "a1, axiom, $distinct" in proof[-1]:
+            proof = proof[:-1]
 
         # Load generated problem
         prob = load_and_process_problem(problem_path, deepmath=False)
-
         if len(prob) < 1:
             # Report and skip empty problems
             print(f'Warning: empty problem {prob}')
             continue
-
+        # Skip the conjecture to make a jaccard score of 1 possible
+        if 'conjecture' in prob[0]:
+            prob = prob[1:]
 
         results[name] = {"jaccard": jaccard_score_np([proof], [prob]),
                          "coverage": coverage_score_np([proof], [prob]),
@@ -68,17 +72,12 @@ def run_initial_analysis(exp_id, problem_dir):
         # Perfromance only contains solved entries
         if prob_name in performance:
             data[prob_name] = {**metrics[prob_name], **performance[prob_name]}
-        #data[prob_name] = {**metrics[prob_name]}
 
     # Convert to pandas - problem names used as index
     df = pd.DataFrame.from_dict(data).T
 
-    print(df)
-
-
-    # Report staistics
-    # E.g. number of problems solved
-    # Only compute for solved problems???
+    # Report the statistics
+    print(df.describe())
 
 
 def get_performance_stats(exp_id):
