c2fd45e29c63234d79bfe4ec1ee02d451a5841a1
Edvard Holden <edvardholden@gmail.com>
2022-10-03 13:42:11 +0100

    Add important run scripts

---
+++ run_generate_sine.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+python3 -u generate_problems.py --mode sine --output_format original  --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 1 --sine_st 1
+#python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 1 --sine_st 1.5
+python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 1 --sine_st 2
+#python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 1 --sine_st 2.5
+#
+#
+python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 2 --sine_st 1
+#python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 2 --sine_st 1.5
+python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 2 --sine_st 2
+#python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 2 --sine_st 2.5
+#
+python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 4 --sine_st 1
+#python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 4 --sine_st 1.5
+python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 4 --sine_st 2
+#python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 4 --sine_st 2.5
+#
+python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 8 --sine_st 1
+#python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 8 --sine_st 1.5
+python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 8 --sine_st 2
+#python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 8 --sine_st 2.5
+#
+python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 0 --sine_st 1
+#python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 0 --sine_st 1.5
+python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 0 --sine_st 2
+#python3 -u generate_problems.py --mode sine --output_format original --problem_dir ~/gnn-entailment-caption/merged_problems/ --problem_format mptp --sine_sd 0 --sine_st 2.5
+
+
+echo "# FINSIHED #"
